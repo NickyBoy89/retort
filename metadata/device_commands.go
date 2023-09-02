05fd9c092c2177a173a4279ddd8461a0a2730bc4
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-09-02 15:02:39 -0700

    feat: Added additional device commands to upload a file buffer

--- metadata/device_commands.go
+++ metadata/device_commands.go
@@ -3,6 +3,7 @@ package metadata
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,7 +14,7 @@ import (
 	"github.com/google/uuid"
 )
 
-// `listMetadataFiles` looks in the device and returns a list of all the
+// ListMetadataFiles looks in the device and returns a list of all the
 // file names for the metadata files
 func ListMetadataFiles() ([]string, error) {
 	entries, err := os.ReadDir(MetadataDir)
@@ -36,75 +37,105 @@ func ListMetadataFiles() ([]string, error) {
 	return fileNames, nil
 }
 
+// createSupplimentalFiles is an internal function that creates all of the
+// necessary files for uploading a file to the reMarkable device
+//
+// `fileId` should be the UUID of the file being uploaded
+//
+// The final upload of the actual data file needs to be handled separately, as
+// well as the metadate for that file
+func createSupplimentalFiles(fileUUID uuid.UUID) error {
+	fileId := fileUUID.String()
+	destFolder := filepath.Join(MetadataDir, fileId)
+	thumbnailFolder := filepath.Join(MetadataDir, fileId+".thumbnails")
+	contentFile := filepath.Join(MetadataDir, fileId+".content")
+
+	// Create a folder
+	log.Debugf("Creating new file directory at %s", destFolder)
+	if err := os.MkdirAll(destFolder, 0755); err != nil {
+		return err
+	}
+
+	// Make a folder for the thumbnails
+	log.Debugf("Creating new thumbnail folder at %s", thumbnailFolder)
+	if err := os.MkdirAll(thumbnailFolder, 0755); err != nil {
+		return err
+	}
+
+	// Upload a blank content file
+	log.Debugf("Creating new content file at %s", contentFile)
+	if err := os.WriteFile(contentFile, []byte("{}"), 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ReloadFiles restarts the reMarkable's user interface, which allows for newly
+// uploaded files to be shown immediately
 func ReloadFiles() error {
 	return exec.Command("systemctl", "restart", "xochitl").Run()
 }
 
-func UploadFile(fileName string, writeFiles bool) error {
+// UploadFileBuffer uploads a file, given by its buffer, to the reMarkable device
+//
+// Detection of the file's type is done through specifying the file's name
+func UploadFileBuffer(fileName string, fileBuffer io.Reader) error {
 	switch filepath.Ext(fileName) {
 	case ".pdf":
 		// To upload a file, we need to complete several steps:
 
 		fileId := uuid.New()
-		destFolder := filepath.Join(MetadataDir, fileId.String())
-		thumbnailFolder := filepath.Join(MetadataDir, fileId.String()+".thumbnails")
-		contentFile := filepath.Join(MetadataDir, fileId.String()+".content")
+
+		// Create the supplimentary files
+		createSupplimentalFiles(fileId)
+
+		// Now, we only need to handle the upload of the final file, and the metadata
 		metadataFile := filepath.Join(MetadataDir, fileId.String()+".metadata")
 		outputFile := filepath.Join(MetadataDir, fileId.String()+filepath.Ext(fileName))
 
-		fmt.Printf("Making new directory %s\n", destFolder)
-		fmt.Printf("Making new thumbnail directory %s\n", thumbnailFolder)
-		fmt.Printf("Making new content file %s\n", contentFile)
-		fmt.Printf("Making new metadata file %s\n", metadataFile)
-		fmt.Printf("Copying file from %s to %s\n", fileName, outputFile)
+		// Upload the relevant metadata file
+		metadata := NewMetadataForFile(fileName)
 
-		if writeFiles {
-			log.Warn("Writing files")
-			// Create a folder
-			if err := os.MkdirAll(destFolder, 0755); err != nil {
-				return err
-			}
-			// Make a folder for the thumbnails
-			if err := os.MkdirAll(thumbnailFolder, 0755); err != nil {
-				return err
-			}
-			// Upload a blank content file
-			if err := os.WriteFile(contentFile, []byte("{}"), 0644); err != nil {
-				return err
-			}
+		outputMetadata, err := os.OpenFile(metadataFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			return err
+		}
+		defer outputMetadata.Close()
 
-			// Upload the relevant metadata file
-			metadata := NewMetadataForFile(fileName)
+		encoder := json.NewEncoder(outputMetadata)
+		encoder.SetIndent("", "    ")
+		if err := encoder.Encode(metadata); err != nil {
+			return err
+		}
 
-			metadataFile, err := os.OpenFile(metadataFile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-			if err != nil {
-				return err
-			}
-			defer metadataFile.Close()
+		// Upload the document file to the correct location
+		destFile, err := os.OpenFile(outputFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+		if err != nil {
+			return err
+		}
+		defer destFile.Close()
 
-			encoder := json.NewEncoder(metadataFile)
-			encoder.SetIndent("", "    ")
-			if err := encoder.Encode(metadata); err != nil {
-				return err
-			}
+		if _, err := io.Copy(destFile, fileBuffer); err != nil {
+			return err
+		}
 
-			// Upload the file to the correct location
-			contents, err := os.ReadFile(fileName)
-			if err != nil {
-				return err
-			}
-
-			if err := os.WriteFile(outputFile, contents, 0600); err != nil {
-				return err
-			}
-
-			if err := ReloadFiles(); err != nil {
-				return err
-			}
+		if err := ReloadFiles(); err != nil {
+			return err
 		}
 	default:
 		return fmt.Errorf("Unknown file type to upload: %s", fileName)
 	}
 
 	return nil
+}
+
+func UploadFile(fileName string) error {
+	fileBuffer, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer fileBuffer.Close()
+
+	return UploadFileBuffer(fileName, fileBuffer)
 }
