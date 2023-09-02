4c60c0c6b5036221cad13050ca01dd939a0762d7
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-09-02 15:26:05 -0700

    feat: Keep waiting for new files, instead of exiting

--- receive_files.go
+++ receive_files.go
@@ -17,30 +17,30 @@ var ReceiveFilesCommand = &cobra.Command{
 
 		log.Info("Waiting to receive any files")
 
-		files, err := client.WaitingFiles(context.Background())
-		if err != nil {
-			return err
-		}
-
-		log.Infof("Received file list: %v", files)
-
-		for _, file := range files {
-			log.Infof("Fetching waiting file %s", file.Name)
-			fileBuf, _, err := client.GetWaitingFile(context.Background(), file.Name)
+		for {
+			files, err := client.WaitingFiles(context.Background())
 			if err != nil {
 				return err
 			}
 
-			log.Info("Uploading to device")
-			if err := metadata.UploadFileBuffer(file.Name, fileBuf); err != nil {
-				return err
+			log.Infof("Received file list: %v", files)
+
+			for _, file := range files {
+				log.Infof("Fetching waiting file %s", file.Name)
+				fileBuf, _, err := client.GetWaitingFile(context.Background(), file.Name)
+				if err != nil {
+					return err
+				}
+
+				log.Info("Uploading to device")
+				if err := metadata.UploadFileBuffer(file.Name, fileBuf); err != nil {
+					return err
+				}
+
+				log.Info("Done")
+
+				fileBuf.Close()
 			}
-
-			log.Info("Done")
-
-			fileBuf.Close()
 		}
-
-		return nil
 	},
 }
